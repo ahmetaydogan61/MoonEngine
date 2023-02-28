e7834c9d666f67d97e5f16276e5d66ca4616e5b7
Ahmet Aydogan <ahmetaydogancode@gmail.com>
2023-02-28 16:44:02 +0300

    Renamed static variables

--- MoonEngine/Source/Renderer/Renderer.cpp
+++ MoonEngine/Source/Renderer/Renderer.cpp
@@ -13,21 +13,22 @@
 
 namespace MoonEngine
 {
-	bool Renderer::m_RendererInitialized = false;
+	bool Renderer::s_RendererInitialized = false;
 	
-	uint32_t Renderer::m_VertexArray = 0;
-	uint32_t Renderer::m_VertexBuffer = 0;
-	uint32_t Renderer::m_IndexBuffer = 0;
-	uint32_t Renderer::m_VertexIndex = 0;
-	Vertex* Renderer::m_Vertices = nullptr;
+	uint32_t Renderer::s_VertexArray = 0;
+	uint32_t Renderer::s_VertexBuffer = 0;
+	uint32_t Renderer::s_IndexBuffer = 0;
+	uint32_t Renderer::s_VertexIndex = 0;
+	Vertex* Renderer::s_Vertices = nullptr;
 	
-	Shared<Shader> Renderer::m_Shader = nullptr;
-	Shared<Texture> Renderer::m_DefaultTexture = nullptr;
-	RendererData Renderer::m_RendererData = {};
+	Shared<Shader> Renderer::s_Shader = nullptr;
+	Shared<Texture> Renderer::s_DefaultTexture = nullptr;
+	RendererData Renderer::s_RendererData = {};
 
-	uint32_t Renderer::m_TextureIndex = 0;
-	int32_t Renderer::m_TextureIds[32];
-	std::unordered_map<Shared<Texture>, int32_t> Renderer::m_TextureCache;
+	uint32_t Renderer::s_TextureIndex = 0;
+	int32_t Renderer::s_TextureIds[32];
+	std::unordered_map<Shared<Texture>, int32_t> Renderer::s_TextureCache
+		;
 
 	const glm::vec4 VertexPositions[4] =
 	{
@@ -47,19 +48,27 @@ namespace MoonEngine
 
 	void Renderer::Init()
 	{
-		m_Shader = MakeShared<Shader>("Resource/Shaders/Default.shader");
-		m_DefaultTexture = MakeShared<Texture>();
+		if (s_RendererInitialized)
+		{
+			ME_LOG("Trying to initialize renderer again!");
+			return;
+		}
 
-		m_Vertices = new Vertex[m_RendererData.MaxVertexCount];
+		s_RendererInitialized = true;
 
-		glGenVertexArrays(1, &m_VertexArray);
-		glGenBuffers(1, &m_VertexBuffer);
-		glGenBuffers(1, &m_IndexBuffer);
+		s_Shader = MakeShared<Shader>("Resource/Shaders/Default.shader");
+		s_DefaultTexture = MakeShared<Texture>();
 
-		glBindVertexArray(m_VertexArray);
+		s_Vertices = new Vertex[s_RendererData.MaxVertexCount];
 
-		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * m_RendererData.MaxVertexCount, m_Vertices, GL_DYNAMIC_DRAW);
+		glGenVertexArrays(1, &s_VertexArray);
+		glGenBuffers(1, &s_VertexBuffer);
+		glGenBuffers(1, &s_IndexBuffer);
+
+		glBindVertexArray(s_VertexArray);
+
+		glBindBuffer(GL_ARRAY_BUFFER, s_VertexBuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * s_RendererData.MaxVertexCount, s_Vertices, GL_DYNAMIC_DRAW);
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
 		glEnableVertexAttribArray(0);
@@ -79,10 +88,10 @@ namespace MoonEngine
 		glVertexAttribIPointer(5, 1, GL_INT, sizeof(Vertex), (void*)offsetof(Vertex, EntityId));
 		glEnableVertexAttribArray(5);
 
-		uint32_t* indices = new uint32_t[m_RendererData.MaxIndexCount];
+		uint32_t* indices = new uint32_t[s_RendererData.MaxIndexCount];
 		uint32_t indicesIndex = 0;
 
-		for (uint32_t i = 0; i < m_RendererData.MaxIndexCount; i += 6)
+		for (uint32_t i = 0; i < s_RendererData.MaxIndexCount; i += 6)
 		{
 			indices[i + 0] = 0 + indicesIndex;
 			indices[i + 1] = 1 + indicesIndex;
@@ -96,23 +105,23 @@ namespace MoonEngine
 		}
 
 		for (int i = 0; i < 32; i++)
-			m_TextureIds[i] = i;
-		m_TextureCache.reserve(32);
+			s_TextureIds[i] = i;
+		s_TextureCache.reserve(32);
 
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * m_RendererData.MaxIndexCount, indices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, s_IndexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * s_RendererData.MaxIndexCount, indices, GL_STATIC_DRAW);
 
 		delete[] indices;
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-		m_RendererInitialized = true;
+		s_RendererInitialized = true;
 	}
 
 	void Renderer::SetRenderData(const glm::mat4& viewProjection)
 	{
-		m_RendererData.ViewProjection = viewProjection;
+		s_RendererData.ViewProjection = viewProjection;
 	}
 
 	void Renderer::SetRenderMode(RenderMode renderMode)
@@ -131,7 +140,7 @@ namespace MoonEngine
 	void Renderer::SetClearColor(const glm::vec3& color)
 	{
 		glClearColor(color.x, color.y, color.z, 1.0f);
-		m_RendererData.ClearColor = color;
+		s_RendererData.ClearColor = color;
 	}
 
 	void Renderer::Clear()
@@ -143,38 +152,38 @@ namespace MoonEngine
 	{
 		Clear();
 
-		m_RendererData.DrawCalls = 0;
-		m_RendererData.VertexCount = 0;
-		m_RendererData.QuadCount = 0;
+		s_RendererData.DrawCalls = 0;
+		s_RendererData.VertexCount = 0;
+		s_RendererData.QuadCount = 0;
 
-		m_Shader->SetMat4("uVP", m_RendererData.ViewProjection);
-		m_Shader->SetIntArray("uTexture", 32, m_TextureIds);
+		s_Shader->SetMat4("uVP", s_RendererData.ViewProjection);
+		s_Shader->SetIntArray("uTexture", 32, s_TextureIds);
 	}
 
 	void Renderer::End()
 	{
-		m_Shader->Bind();
-		m_DefaultTexture->Bind(0);
+		s_Shader->Bind();
+		s_DefaultTexture->Bind(0);
 
 		Render();
-		m_VertexIndex = 0; 
-		m_TextureIndex = 0;
-		m_TextureCache.clear();
+		s_VertexIndex = 0; 
+		s_TextureIndex = 0;
+		s_TextureCache.clear();
 	}
 
 	void Renderer::Render()
 	{
-		if (m_VertexIndex < 4)
+		if (s_VertexIndex < 4)
 			return;
 
-		glBindVertexArray(m_VertexArray);
+		glBindVertexArray(s_VertexArray);
 		//Obsolete
 		//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * s_VertexIndex, s_Vertices, GL_DYNAMIC_DRAW);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * m_VertexIndex, m_Vertices);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertex) * s_VertexIndex, s_Vertices);
 
-		glDrawElements(GL_TRIANGLES, m_RendererData.QuadCount * 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, s_RendererData.QuadCount * 6, GL_UNSIGNED_INT, 0);
 
-		m_RendererData.DrawCalls++;
+		s_RendererData.DrawCalls++;
 	}
 
 	void Renderer::DrawQuad(const glm::vec3& position, const glm::vec3& rotation, const glm::vec3& scale,
@@ -188,25 +197,25 @@ namespace MoonEngine
 
 	void Renderer::DrawQuad(const glm::mat4& transform, const Shared<Texture>& texture, const glm::vec4& color, const glm::vec2& tiling)
 	{
-		if (m_VertexIndex >= m_RendererData.MaxVertexCount || m_TextureIndex >= 32)
+		if (s_VertexIndex >= s_RendererData.MaxVertexCount || s_TextureIndex >= 32)
 			End();
 
 		for (int i = 0; i < 4; i++)
 		{
-			m_Vertices[m_VertexIndex + i].Position = transform * VertexPositions[i];
-			m_Vertices[m_VertexIndex + i].Color = color;
-			m_Vertices[m_VertexIndex + i].TextureCoord = TexCoords[i];
-			m_Vertices[m_VertexIndex + i].Tiling = tiling;
-			m_Vertices[m_VertexIndex + i].EntityId = -1;
+			s_Vertices[s_VertexIndex + i].Position = transform * VertexPositions[i];
+			s_Vertices[s_VertexIndex + i].Color = color;
+			s_Vertices[s_VertexIndex + i].TextureCoord = TexCoords[i];
+			s_Vertices[s_VertexIndex + i].Tiling = tiling;
+			s_Vertices[s_VertexIndex + i].EntityId = -1;
 
 			if (!texture)
-				m_Vertices[m_VertexIndex + i].TextureId = 0;
+				s_Vertices[s_VertexIndex + i].TextureId = 0;
 			else
-				m_Vertices[m_VertexIndex + i].TextureId = GetTextureFromCache(texture);
+				s_Vertices[s_VertexIndex + i].TextureId = GetTextureFromCache(texture);
 		}
-		m_VertexIndex += 4;
-		m_RendererData.VertexCount = m_VertexIndex;
-		m_RendererData.QuadCount++;
+		s_VertexIndex += 4;
+		s_RendererData.VertexCount = s_VertexIndex;
+		s_RendererData.QuadCount++;
 	}
 
 	void Renderer::DrawEntity(const glm::mat4& transform, const SpriteComponent& spriteComponent, int entityId)
@@ -225,47 +234,55 @@ namespace MoonEngine
 
 	void Renderer::DrawEntity(const glm::mat4& transform, const Shared<Texture>& texture, const glm::vec4& color, const glm::vec2& tiling, int entityId)
 	{
-		if (m_VertexIndex >= m_RendererData.MaxVertexCount || m_TextureIndex >= 32)
+		if (s_VertexIndex >= s_RendererData.MaxVertexCount || s_TextureIndex >= 32)
 			End();
 
 		for (int i = 0; i < 4; i++)
 		{
-			m_Vertices[m_VertexIndex + i].Position = transform * VertexPositions[i];
-			m_Vertices[m_VertexIndex + i].Color = color;
-			m_Vertices[m_VertexIndex + i].TextureCoord = TexCoords[i];
-			m_Vertices[m_VertexIndex + i].Tiling = tiling;
-			m_Vertices[m_VertexIndex + i].EntityId = entityId;
+			s_Vertices[s_VertexIndex + i].Position = transform * VertexPositions[i];
+			s_Vertices[s_VertexIndex + i].Color = color;
+			s_Vertices[s_VertexIndex + i].TextureCoord = TexCoords[i];
+			s_Vertices[s_VertexIndex + i].Tiling = tiling;
+			s_Vertices[s_VertexIndex + i].EntityId = entityId;
 
 			if (!texture)
-				m_Vertices[m_VertexIndex + i].TextureId = 0;
+				s_Vertices[s_VertexIndex + i].TextureId = 0;
 			else
-				m_Vertices[m_VertexIndex + i].TextureId = GetTextureFromCache(texture);
+				s_Vertices[s_VertexIndex + i].TextureId = GetTextureFromCache(texture);
 		}
-		m_VertexIndex += 4;
-		m_RendererData.VertexCount = m_VertexIndex;
-		m_RendererData.QuadCount++;
+		s_VertexIndex += 4;
+		s_RendererData.VertexCount = s_VertexIndex;
+		s_RendererData.QuadCount++;
 	}
 
 	int32_t Renderer::GetTextureFromCache(const Shared<Texture>& texture)
 	{
-		if (m_TextureCache.find(texture) != m_TextureCache.end())
-			return m_TextureCache.at(texture);
+		if (s_TextureCache.find(texture) != s_TextureCache.end())
+			return s_TextureCache.at(texture);
 
-		m_TextureIndex++;
-		texture->Bind(m_TextureIndex);
-		m_TextureCache[texture] = m_TextureIndex;
-		return m_TextureIndex;
+		s_TextureIndex++;
+		texture->Bind(s_TextureIndex);
+		s_TextureCache[texture] = s_TextureIndex;
+		return s_TextureIndex;
 	}
 
 	void Renderer::Terminate()
 	{
-		glDeleteVertexArrays(1, &m_VertexArray);
-		glDeleteBuffers(1, &m_VertexBuffer);
-		glDeleteBuffers(1, &m_IndexBuffer);
+		if (!s_RendererInitialized)
+		{
+			ME_SYS_WAR("Trying to terminate non initialized Renderer!");
+			return;
+		}
 
-		delete[] m_Vertices;
-		m_TextureIndex = 0;
-		m_VertexIndex = 0;
-		m_TextureCache.clear();
+		glDeleteVertexArrays(1, &s_VertexArray);
+		glDeleteBuffers(1, &s_VertexBuffer);
+		glDeleteBuffers(1, &s_IndexBuffer);
+
+		delete[] s_Vertices;
+		s_TextureIndex = 0;
+		s_VertexIndex = 0;
+		s_TextureCache.clear();
+
+		s_RendererInitialized = false;
 	}
 }
